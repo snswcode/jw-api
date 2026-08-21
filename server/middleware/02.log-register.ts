4950f49e9a618a573dc74e6e147b3f109ac46f65
snswcode <SoftnadoSoftware@Gmail.com>
2026-08-21 08:12:04 -0400

    fix: installed lint then performed its auto-fix prettier formatting in log middleware

--- server/middleware/02.log-register.ts
+++ server/middleware/02.log-register.ts
@@ -5,16 +5,17 @@ export default fromNodeMiddleware(
     autoLogging: false,
     genReqId: (req) => req.headers['x-tracing-id'] || req.id,
     level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
-    transport: process.env.NODE_ENV !== 'production'
-      ? {
-          options: {
-            colorize: true,
-            ignore: 'req,res,responseTime,pid,hostname',
-            messageFormat: '[reqId:{req.id}] - {msg}',
-            translateTime: 'SYS:standard'
-          },
-          target: 'pino-pretty'
-        }
-      : undefined
+    transport:
+      process.env.NODE_ENV !== 'production'
+        ? {
+            options: {
+              colorize: true,
+              ignore: 'req,res,responseTime,pid,hostname',
+              messageFormat: '[reqId:{req.id}] - {msg}',
+              translateTime: 'SYS:standard'
+            },
+            target: 'pino-pretty'
+          }
+        : undefined
   })
 )
